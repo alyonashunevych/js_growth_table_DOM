b10480f6937f3dc7584687c42ced543a06c57a54
Alyona Shunevych <olena17ju06@gmail.com>
2025-02-27 21:57:25 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,70 @@
 'use strict';
 
-// write code here
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+const table = document.querySelector('.field').firstElementChild;
+const rows = table.children;
+const MAXCOUNT = 10;
+const MINCOUNT = 2;
+
+appendRowButton.addEventListener('click', (e) => {
+  const newRow = rows[0].cloneNode(true);
+
+  table.append(newRow);
+
+  if (rows.length > MINCOUNT) {
+    removeRowButton.removeAttribute('disabled');
+  }
+
+  if (rows.length >= MAXCOUNT) {
+    appendRowButton.setAttribute('disabled', 'disabled');
+  }
+});
+
+removeRowButton.addEventListener('click', (e) => {
+  const lastRow = table.lastElementChild;
+
+  lastRow.remove();
+
+  if (rows.length < MAXCOUNT) {
+    appendRowButton.removeAttribute('disabled');
+  }
+
+  if (rows.length <= MINCOUNT) {
+    removeRowButton.setAttribute('disabled', 'disabled');
+  }
+});
+
+appendColumnButton.addEventListener('click', (e) => {
+  [...rows].forEach((row) => {
+    const newCell = row.firstElementChild.cloneNode(true);
+
+    row.append(newCell);
+  });
+
+  if (rows[0].children.length > MINCOUNT) {
+    removeColumnButton.removeAttribute('disabled');
+  }
+
+  if (rows[0].children.length >= MAXCOUNT) {
+    appendColumnButton.setAttribute('disabled', 'disabled');
+  }
+});
+
+removeColumnButton.addEventListener('click', (e) => {
+  [...rows].forEach((row) => {
+    const lastCell = row.lastElementChild;
+
+    lastCell.remove();
+  });
+
+  if (rows[0].children.length < MAXCOUNT) {
+    appendColumnButton.removeAttribute('disabled');
+  }
+
+  if (rows[0].children.length <= MINCOUNT) {
+    removeColumnButton.setAttribute('disabled', 'disabled');
+  }
+});
